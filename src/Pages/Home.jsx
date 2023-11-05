abf91453d6185899f8921286983822ccbcf230ca
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-05 16:58:07 -0500

    makes small changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 function Home() {
     return (
       <div className="home-container">
-      <h2 className="welcome-heading">Welcome</h2>
-      <h3 className="app-heading">To Alex Tsiklidis' Budgeting App!</h3>
+      <h2 className="welcome-heading">Welcome to</h2>
+      <h3 className="app-heading">Alex Tsiklidis' Budgeting App!</h3>
       
     </div>
     );
